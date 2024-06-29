a68590f6025512f8bde7763d3949c973265381ac
maliaoMJ <1302151931@qq.com>
2024-06-30 02:34:25 +0800

    添加组件库预览

---
+++ packages/components/modal/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="modal">vue modal{{ msg }}</div>
+</template>
+
+<script name="Modal" setup lang="ts">
+  import { ref } from "vue";
+
+  const msg = ref<string>("");
+
+  setTimeout(() => {
+    msg.value = "modal msg";
+  }, 5000);
+
+</script>
+<style lang="scss" scope>
+.modal {
+  width: 100px; 
+  height: 100px;
+  background:red
+}
+</style>
---
+++ packages/components/preview.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+   hello world
+   <Modal></Modal>
+  </div>
+ </template>
+ <script lang='ts' setup name='App'>
+ import {Modal} from './index'
+ </script>
